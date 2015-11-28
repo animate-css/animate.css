d112523ca5aa080d3b0b642e34ff1005fc69ca18
mtbnunu <mtbnunu@gmail.com>
2015-11-27 22:28:45 -0800

    add easing and sidebyside view

--- js/attention_seekers.js
+++ js/attention_seekers.js
@@ -1,6 +1,15 @@
 animateCss.bounce = function (e) {
-    //CODE TO ANIMATE bounce
-    alert("bounce " + e);
+    var mt = $(e).css("marginTop");
+    var mb = $(e).css("paddingBottom")
+    $(e).animate({
+        marginTop: "-=30",
+        paddingBottom: "+=30",
+    }, 300, "easeInOutQuart", function () {
+        $(e).animate({
+            marginTop: mt,
+            paddingBottom: mb
+        }, 800, "easeOutBounce")
+    })
 }
 
 animateCss.flash = function (e) {
