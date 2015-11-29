4fa1c1dd53a40e52998038380bed7c72cbe3d4dd
JamieRL <lee.jamr@gmail.com>
2015-11-28 18:29:30 -0800

    added fadeOutLeft and fadeOutLeftBig

--- js/fading_exits.js
+++ js/fading_exits.js
@@ -5,17 +5,15 @@ animateCss.fadeOut = function (e) {
 animateCss.fadeOutDown = function (e){
     var pt = $(e).css("paddingTop");
     var mb = $(e).css("marginBottom");
-    console.log(pt);
     $(e).fadeOut(1000).animate({
             paddingTop: "+=100",
             marginBottom: "-=100"
-        }, {duration: 1000,queue: false, easing: "swing", complete: fadeOutReset(e,pt,mb)});
+        }, {duration: 1000,queue: false, easing: "easeOutQuad", complete: fadeOutDownReset(e,pt,mb)});
 
 }
 
-function fadeOutReset(e,pt,mb){
+function fadeOutDownReset(e,pt,mb){
     return function(){
-                console.log(pt);
                 $(e).css('paddingTop', pt);
                 $(e).css('marginBottom',mb);
                 $(e).fadeIn(1);
@@ -25,43 +23,35 @@ function fadeOutReset(e,pt,mb){
 animateCss.fadeOutDownBig = function (e){
     var pt = $(e).css("paddingTop");
     var mb = $(e).css("marginBottom");
-    console.log(pt);
     $(e).fadeOut(1000).animate({
             paddingTop: "+=1000",
             marginBottom: "-=1000"
-        }, {duration: 900,queue: false, easing: "swing", complete: fadeOutReset(e,pt,mb)});
+        }, {duration: 900,queue: false, easing: "swing", complete: fadeOutDownReset(e,pt,mb)});
 }
 
 animateCss.fadeOutLeft = function (e){
-    var ml = $(e).css("marginLeft");
     var pr = $(e).css("paddingRight");
-    $(e).fadeOut(1);
-    $(e).animate({
-        marginLeft: "-=200",
-        paddingRight: "+=200",
-    }, 1, "linear", function () {
-        $(e).fadeIn(1000).animate({
-            marginLeft: ml,
-            paddingRight: pr
-        }, {duration: 900,queue: false}, "swing",function(){
-    });
-    });
+    var ml = $(e).css("marginLeft");
+    $(e).fadeOut(1000).animate({
+            paddingRight: "+=150",
+            marginLeft: "-=150"
+        }, {duration: 800,queue: false, easing: "swing", complete: fadeOutLeftReset(e,pr,ml)});
 }
 
 animateCss.fadeOutLeftBig = function (e){
-    var ml = $(e).css("marginLeft");
     var pr = $(e).css("paddingRight");
-    $(e).fadeOut(1);
-    $(e).animate({
-        marginLeft: "-=1500",
-        paddingRight: "+=1500",
-    }, 1, "linear", function () {
-        $(e).fadeIn(1000).animate({
-            marginLeft: ml,
-            paddingRight: pr
-        }, {duration: 900,queue: false}, "swing",function(){
-    });
-    });
+    var ml = $(e).css("marginLeft");
+    $(e).fadeOut(500).animate({
+            paddingRight: "+=1000",
+            marginLeft: "-=1000"
+        }, {duration: 1000,queue: false, easing: "easeOutCubic", complete: fadeOutLeftReset(e,pr,ml)});
+}
+function fadeOutLeftReset(e,pr,ml){
+    return function(){
+                $(e).css('paddingRight', pr);
+                $(e).css('marginLeft',ml);
+                $(e).fadeIn(1);
+    }
 }
 
 animateCss.fadeOutRight = function (e){
