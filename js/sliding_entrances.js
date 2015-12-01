f72e117a614330642ee6b4faab48c980932a0c53
Matthew Piaseczny <mpiaseczny@alumni.ubc.ca>
2015-12-01 12:04:08 -0800

    Changed values to more similarly represent the CSS

--- js/sliding_entrances.js
+++ js/sliding_entrances.js
@@ -3,13 +3,13 @@ animateCss.slideInUp = function (e){
     var mb = $(e).css("marginBottom");
     var pt = $(e).css("paddingTop");
     $(e).animate({
-        marginBottom: "-=90",
-        paddingTop: "+=90",
+        marginBottom: "-=95",
+        paddingTop: "+=95",
     }, 5, "linear", function () {
         $(e).animate({
             marginBottom: mb,
             paddingTop: pt
-        }, {duration: 1000,queue: false}, "swing",function(){
+        }, {duration: 900,queue: false}, "linear",function(){
     });
     });
 } 
@@ -18,13 +18,13 @@ animateCss.slideInDown = function (e) {
     var mt = $(e).css("marginTop");
     var pb = $(e).css("paddingBottom");
     $(e).animate({
-        marginTop: "-=90",
-        paddingBottom: "+=90",
+        marginTop: "-=95",
+        paddingBottom: "+=95",
     }, 5, "linear", function () {
         $(e).animate({
             marginTop: mt,
             paddingBottom: pb
-        }, {duration: 1000,queue: false}, "swing",function(){
+        }, {duration: 900,queue: false}, "linear",function(){
     });
     });
 } 
@@ -33,13 +33,13 @@ animateCss.slideInLeft = function (e) {
     var ml = $(e).css("marginLeft");
     var pr = $(e).css("paddingRight");
     $(e).animate({
-        marginLeft: "-=90",
-        paddingRight: "+=90",
+        marginLeft: "-=190",
+        paddingRight: "+=190",
     }, 5, "linear", function () {
         $(e).animate({
             marginLeft: ml,
             paddingRight: pr
-        }, {duration: 1000,queue: false}, "swing",function(){
+        }, {duration: 900,queue: false}, "linear",function(){
     });
     });
 } 
@@ -48,13 +48,13 @@ animateCss.slideInRight = function (e) {
     var mr = $(e).css("marginRight");
     var pl = $(e).css("paddingLeft");
     $(e).animate({
-        marginRight: "-=90",
-        paddingLeft: "+=90",
+        marginRight: "-=190",
+        paddingLeft: "+=190",
     }, 5, "linear", function () {
         $(e).animate({
             marginRight: mr,
             paddingLeft: pl
-        }, {duration: 1000,queue: false}, "swing",function(){
+        }, {duration: 900,queue: false}, "linear",function(){
     });
     });
 } 
