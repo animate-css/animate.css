1628e21163fa464a5796beddaf1e335e72383ab7
JamieRL <lee.jamr@gmail.com>
2015-12-01 19:03:23 -0800

    completed rotating entrances

--- js/rotating_entrances.js
+++ js/rotating_entrances.js
@@ -1,5 +1,6 @@
 animateCss.rotateIn = function(e){
     console.log("called");
+    $(e).css('transform-origin','center');
     $(e).css('-webkit-transform', "rotateX(180deg)");
     $(e).fadeOut(0).fadeIn(700).animate({  asdf: 180 }, {
     step: function(go) {
@@ -20,26 +21,90 @@ animateCss.rotateIn = function(e){
 }
 //need to first flip it then animate it back
 
-/*
-animateCss.bounceIn = function (e) {
-    $(e).animate({ asdf: 0 },0)
-        .animate({asdf: 1}, {
-        step: function (now, fx) {
-            $(this).css('-webkit-transform', "scale(" + now + ")");
+animateCss.rotateInDownLeft = function(e){
+    console.log("called");
+    //$(e).css('-webkit-transform', "rotateX(180deg)");
+    $(e).css('transform-origin', 'left bottom');
+    $(e).fadeOut(0).fadeIn(900).animate({  asdf: 315 }, {
+    step: function(go) {
+      $(this).css('-webkit-transform','rotate('+go+'deg)');
+    },
+    duration: 10,queue: false, easing: 'linear',
+    complete: function(){ 
+        $(e).animate({  asdf: 360 }, {
+        step: function(go) {
+            $(this).css('-webkit-transform','rotate('+go+'deg)');
         },
-        duration: 1000,
-        easing: "easeOutElastic"
-    }, "easeOutElastic")
+        duration: 700,queue: false, easing: 'linear',
+        complete: function(){ }} 
+        );
+    }
+    });
+
 }
 
-$('#myDiv').animate({ textIndent: 0 }, {
+animateCss.rotateInDownRight = function(e){
+    console.log("called");
+    //$(e).css('-webkit-transform', "rotateX(180deg)");
+    $(e).css('transform-origin', 'right bottom');
+    $(e).fadeOut(0).fadeIn(900).animate({  asdf: -315 }, {
     step: function(go) {
-      $(this).css('-moz-transform','rotate('+go+'deg)');
       $(this).css('-webkit-transform','rotate('+go+'deg)');
-      $(this).css('-o-transform','rotate('+go+'deg)');
-      $(this).css('transform','rotate('+go+'deg)');
     },
-    duration: 500,
-    complete: function(){ alert('done') }
-});
-*/
+    duration: 10,queue: false, easing: 'linear',
+    complete: function(){ 
+        $(e).animate({  asdf: -360 }, {
+        step: function(go) {
+            $(this).css('-webkit-transform','rotate('+go+'deg)');
+        },
+        duration: 700,queue: false, easing: 'linear',
+        complete: function(){ }} 
+        );
+    }
+    });
+
+}
+
+animateCss.rotateInUpLeft = function(e){
+    console.log("called");
+    //$(e).css('-webkit-transform', "rotateX(180deg)");
+    $(e).css('transform-origin', 'left bottom');
+    $(e).fadeOut(0).fadeIn(900).animate({  asdf: -315 }, {
+    step: function(go) {
+      $(this).css('-webkit-transform','rotate('+go+'deg)');
+    },
+    duration: 10,queue: false, easing: 'linear',
+    complete: function(){ 
+        $(e).animate({  asdf: -360 }, {
+        step: function(go) {
+            $(this).css('-webkit-transform','rotate('+go+'deg)');
+        },
+        duration: 700,queue: false, easing: 'linear',
+        complete: function(){ }} 
+        );
+    }
+    });
+
+}
+
+animateCss.rotateInUpRight = function(e){
+    console.log("called");
+    //$(e).css('-webkit-transform', "rotateX(180deg)");
+    $(e).css('transform-origin', 'right bottom');
+    $(e).fadeOut(0).fadeIn(900).animate({  asdf: 315 }, {
+    step: function(go) {
+      $(this).css('-webkit-transform','rotate('+go+'deg)');
+    },
+    duration: 10,queue: false, easing: 'linear',
+    complete: function(){ 
+        $(e).animate({  asdf: 360 }, {
+        step: function(go) {
+            $(this).css('-webkit-transform','rotate('+go+'deg)');
+        },
+        duration: 700,queue: false, easing: 'linear',
+        complete: function(){ }} 
+        );
+    }
+    });
+
+}
