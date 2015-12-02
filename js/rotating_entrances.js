bb023a29292ba5c7d4eb89d072d9198608e7196b
JamieRL <lee.jamr@gmail.com>
2015-12-01 18:53:59 -0800

    rotateIn working now

--- js/rotating_entrances.js
+++ js/rotating_entrances.js
@@ -1,7 +1,22 @@
 animateCss.rotateIn = function(e){
-    $(e).fadeOut(0).animate({
-        step: function()
+    console.log("called");
+    $(e).css('-webkit-transform', "rotateX(180deg)");
+    $(e).fadeOut(0).fadeIn(700).animate({  asdf: 180 }, {
+    step: function(go) {
+      $(this).css('-webkit-transform','rotate('+go+'deg)');
+    },
+    duration: 10,queue: false, easing: 'linear',
+    complete: function(){ 
+        $(e).animate({  asdf: 360 }, {
+        step: function(go) {
+            $(this).css('-webkit-transform','rotate('+go+'deg)');
+        },
+        duration: 700,queue: false, easing: 'linear',
+        complete: function(){ }} 
+        );
+    }
     });
+
 }
 //need to first flip it then animate it back
 
@@ -16,4 +31,15 @@ animateCss.bounceIn = function (e) {
         easing: "easeOutElastic"
     }, "easeOutElastic")
 }
+
+$('#myDiv').animate({ textIndent: 0 }, {
+    step: function(go) {
+      $(this).css('-moz-transform','rotate('+go+'deg)');
+      $(this).css('-webkit-transform','rotate('+go+'deg)');
+      $(this).css('-o-transform','rotate('+go+'deg)');
+      $(this).css('transform','rotate('+go+'deg)');
+    },
+    duration: 500,
+    complete: function(){ alert('done') }
+});
 */
