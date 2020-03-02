6f47729d2bc19c8bb1f767689ca7413f7481a1fd
Waren Gonzaga <warengonzaga.dev@gmail.com>
2020-03-03 05:51:49 +0800

    Update lightSpeed animations

---
+++ source/lightspeed/lightSpeedOutLeft.css
@@ -0,0 +1,15 @@
+@keyframes lightSpeedOutLeft {
+  from {
+    opacity: 1;
+  }
+
+  to {
+    transform: translate3d(-100%, 0, 0) skewX(-30deg);
+    opacity: 0;
+  }
+}
+
+.lightSpeedOutLeft {
+  animation-name: lightSpeedOutLeft;
+  animation-timing-function: ease-in;
+}
