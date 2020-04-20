d5b856d307d1a1f110ff9b1d110bd869ec43e8f1
Elton Mesquita <callmeelton@gmail.com>
2020-04-20 13:15:54 +0100

    Improves building process and package.json tidy up

--- postcss.config.js
+++ postcss.config.js
@@ -15,25 +15,32 @@ const header = `
 
   `;
 
-module.exports = ctx => ({
-  map: ctx.options.map,
-  parser: ctx.options.parser,
-  plugins: {
-    'postcss-import': {root: ctx.file.dirname},
-    'postcss-prefixer': {
-      prefix: animateConfig.prefix,
-    },
-    'postcss-preset-env': {
-      autoprefixer: {
-        cascade: false,
+module.exports = ctx => {
+  const prefix = ctx.env === 'compat' ? '' : animateConfig.prefix;
+  const devMessage = `🎉🎉🎉🎉 \nanimate.css ${ctx.env} build was compiled sucessfully! \n`;
+
+  console.log(devMessage);
+
+  return {
+    map: ctx.options.map,
+    parser: ctx.options.parser,
+    plugins: {
+      'postcss-import': {root: ctx.file.dirname},
+      'postcss-prefixer': {
+        prefix,
       },
-      features: {
-        'custom-properties': true,
+      'postcss-preset-env': {
+        autoprefixer: {
+          cascade: false,
+        },
+        features: {
+          'custom-properties': true,
+        },
+      },
+      cssnano: ctx.env === 'production' || ctx.env === 'compat' ? {} : false,
+      'postcss-header': {
+        header,
       },
     },
-    cssnano: ctx.env === 'production' ? {} : false,
-    'postcss-header': {
-      header,
-    },
-  },
-});
+  };
+};
