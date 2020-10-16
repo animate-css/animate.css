07ef3c772499c087bc9b3d10e690618edd569a5d
Tusharprabhu <72095249+Tusharprabhu@users.noreply.github.com>
2020-10-16 23:07:51 +0530

    height scrollbar css

---
+++ git.js
@@ -0,0 +1,6 @@
+let progress = document.getElementById('scrollpath');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function () {
+  let progressHeight = (window.pageYOffset / totalHeight) * 100;
+  progress.style.height = progressHeight + "%"
+}
