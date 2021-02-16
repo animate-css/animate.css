3725eaf86d7db1afa05683bea2544a10abcaabaa
Mehmet Sağır <62952843+mehmetsagir@users.noreply.github.com>
2021-02-16 12:00:26 +0300

    arrangements were made

--- docs/main.mjs
+++ docs/main.mjs
@@ -2,6 +2,7 @@ import buildDocsIndex from './modules/buildDocsIndex.mjs';
 import playground from './modules/playground.mjs';
 import startAnimations from './modules/startAnimations.mjs';
 import toggleOnClick from './modules/toggle.mjs';
+import('./modules/darkMode.mjs');
 
 buildDocsIndex();
 playground();
@@ -13,53 +14,13 @@ toggleOnClick('.docs-index', 'html', 'hamburger-active');
 
 requestAnimationFrame(startAnimations);
 
-
-const darkCheck = document.getElementById('night-light-checkbox');
-const moon = document.getElementById('moon-svg');
-const sun = document.getElementById('sun-svg');
-const backToTopBtn = document.querySelector('.back-to-top');
-
 document.querySelectorAll('.copy-icon').forEach(icon => {
-    icon.addEventListener('click', () => {
-        icon.classList.add('copied');
-        document.querySelector('.copied .tooltip').textContent = "Copied!";
-        setTimeout(() => { 
-            icon.children[0].textContent = "Copy class name to clipboard"
-            icon.classList.remove('copied') 
-        }, 750)
-    })
+  icon.addEventListener('click', () => {
+    icon.classList.add('copied');
+    document.querySelector('.copied .tooltip').textContent = 'Copied!';
+    setTimeout(() => {
+      icon.children[0].textContent = 'Copy class name to clipboard'
+      icon.classList.remove('copied')
+    }, 750)
+  })
 })
-
-backToTopBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo(0, 0);
-})
-
-darkCheck.addEventListener('click', () => {
-    if(darkCheck.checked){
-        document.body.className = 'dark';
-        localStorage.setItem('animate-css', 'dark');
-        moon.style.fill = '#e0e0e0';
-        sun.style.fill = '#e0e0e0';
-    }else{
-        document.body.className = '';
-        localStorage.removeItem('animate-css');
-        moon.style.fill = '#111';
-        sun.style.fill = '#111';
-    }
-})
-
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 400) {
-    backToTopBtn.style.display = "grid";
-  } else {
-    backToTopBtn.style.display = "none";
-  }
-});
-
-if(localStorage.getItem('animate-css')){
-    document.body.className = 'dark';
-    darkCheck.checked = true;
-    moon.style.fill = '#e0e0e0';
-    sun.style.fill = '#e0e0e0';
-}
