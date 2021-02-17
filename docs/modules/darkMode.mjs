9130c08c5d2f2b68d27b54ee51de31d7607601cb
Mehmet Sağır <62952843+mehmetsagir@users.noreply.github.com>
2021-02-17 20:02:45 +0300

    dark-mode-button-updated

--- docs/modules/darkMode.mjs
+++ docs/modules/darkMode.mjs
@@ -1,24 +1,16 @@
-const darkCheck = document.getElementById('night-light-checkbox'),
-  moon = document.getElementById('moon-svg'),
-  sun = document.getElementById('sun-svg');
+const darkCheck = document.getElementById('night-light-checkbox');
 
 darkCheck.addEventListener('click', () => {
   if (darkCheck.checked) {
     document.body.classList.add('dark');
     localStorage.setItem('animate-css', 'dark');
-    moon.style.fill = '#e0e0e0';
-    sun.style.fill = '#e0e0e0';
   } else {
     document.body.classList.remove('dark');
     localStorage.removeItem('animate-css');
-    moon.style.fill = '#111';
-    sun.style.fill = '#111';
   }
 })
 
 if (localStorage.getItem('animate-css')) {
   document.body.className = 'dark';
   darkCheck.checked = true;
-  moon.style.fill = '#e0e0e0';
-  sun.style.fill = '#e0e0e0';
 }
