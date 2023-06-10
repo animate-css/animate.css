c17f141f0c6439853f4e572551afecccf3920b1e
Ankit <codingank@gmail.com>
2023-06-10 22:32:51 +0530

    copy docText features added

--- docs/main.mjs
+++ docs/main.mjs
@@ -15,13 +15,56 @@ toggleOnClick('.docs-index', 'html', 'hamburger-active');
 
 requestAnimationFrame(startAnimations);
 
-document.querySelectorAll('.copy-icon').forEach(icon => {
+document.querySelectorAll('.copy-icon').forEach((icon) => {
   icon.addEventListener('click', () => {
     icon.classList.add('copied');
     document.querySelector('.copied .tooltip').textContent = 'Copied!';
     setTimeout(() => {
-      icon.children[0].textContent = 'Copy class name to clipboard'
-      icon.classList.remove('copied')
-    }, 750)
-  })
-})
+      icon.children[0].textContent = 'Copy class name to clipboard';
+      icon.classList.remove('copied');
+    }, 750);
+  });
+});
+
+const select = document.querySelectorAll('pre code');
+const copyBtn = document.querySelectorAll('.copy-btn');
+
+function copyText(btn, text, buttons) {
+  btn.addEventListener('click', () => {});
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      buttons.textContent = 'copied';
+      setTimeout(() => {
+        buttons.textContent = 'copy';
+      }, 2000);
+    })
+    .catch((error) => {
+      console.log('failed to copy', error);
+    });
+}
+
+//to copy text when click on text
+select.forEach((item) => {
+  item.addEventListener('click', () => {
+    const doctextCopy = item.textContent;
+    copyBtn.forEach((copyBtn) => {
+      copyText(item, doctextCopy, copyBtn);
+    });
+  });
+
+  item.addEventListener('mouseover', () => {
+    item.style.background = 'rgb(64 62 62)';
+  });
+  item.addEventListener('mouseout', () => {
+    item.style.background = '#2d2d2d';
+  });
+});
+
+//to copy text when click on copy button
+copyBtn.forEach((button) => {
+  button.addEventListener('click', () => {
+    const textCopy = button.previousElementSibling.textContent;
+    copyText(button, textCopy, button);
+  });
+});
